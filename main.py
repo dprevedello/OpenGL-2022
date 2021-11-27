9fbd20154a1a2c8c84ef675e3289c7202f8cb5f6
Daniele Prevedello <dprevedello86@gmail.com>
2021-11-28 00:21:46 +0100

    Creiamo la prima finestra
    Una semplice finestra realizzata con pygame

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import pygame
+from pygame.locals import *
+
+
+def disegna(window):
+    window.fill([255, 255, 255])
+    pygame.display.update()
+
+
 def main():
-    print("Hello world!")
+    pygame.init()
+    window_size = (500, 500)
+    window = pygame.display.set_mode(window_size)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        disegna(window)
+    pygame.quit()
 
 
 if __name__ == "__main__":
