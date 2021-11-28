dfb35aa76c83a2c7753d05077ac62ca12540b8d3
Daniele Prevedello <dprevedello86@gmail.com>
2021-11-28 22:18:19 +0100

    Creiamo il nostro primo canvas OpenGL
    Sostituiamo il contenuto della finestra con un canvas OpenGL che possiamo disegnare tramite la GPU.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
-def disegna(window):
-    window.fill([255, 255, 255])
-    pygame.display.update()
+def disegna():
+    # R=1 G=0 B=0 A=1
+    glClearColor(1, 0, 0, 1)
+    glClear(GL_COLOR_BUFFER_BIT)
+    pygame.display.flip()
 
 
 def main():
     pygame.init()
     window_size = (500, 500)
-    window = pygame.display.set_mode(window_size)
+    window = pygame.display.set_mode(window_size, DOUBLEBUF | OPENGL)
     pygame.display.set_caption("3D Lab - ISIS Ponti")
     pygame.display.set_icon(pygame.image.load('icon.png'))
 
@@ -23,7 +27,7 @@ def main():
                 running = False
             if event.type == pygame.KEYUP and event.unicode == 'q':
                 running = False
-        disegna(window)
+        disegna()
     pygame.quit()
 
 
