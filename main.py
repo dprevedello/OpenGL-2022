1d001412f67e9eacab3e8f255ebe85107ff38bed
Daniele Prevedello <dprevedello86@gmail.com>
2021-11-28 12:35:16 +0100

    Personalizziamo la finestra
    Aggiungiamo un titolo e un'icona alla pagina. Personalizziamo anche gli eventi da tastiera a cui reagisce il programma.

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ def main():
     pygame.init()
     window_size = (500, 500)
     window = pygame.display.set_mode(window_size)
+    pygame.display.set_caption("3D Lab - ISIS Ponti")
+    pygame.display.set_icon(pygame.image.load('icon.png'))
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+                running = False
+            if event.type == pygame.KEYUP and event.unicode == 'q':
                 running = False
         disegna(window)
     pygame.quit()
