99f2e32a970957acdb886a5605cd85d66def22c3
Daniele Prevedello <dprevedello86@gmail.com>
2021-12-01 13:02:38 +0100

    Aggiungiamo lo zoom
    Colleghiamo al movimento della rotella del mouse e ai tasti + e - una trasformazione di traslazione sull'asse z, così da simulare l'effetto zoom.
    Separiamo inoltre la trasformazione prospettica dalla trasformazione di model-view

--- main.py
+++ main.py
@@ -4,18 +4,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-def disegna():
-    # R=0 G=0 B=0 A=1
-    glClearColor(0, 0, 0, 1)
-    glClear(GL_COLOR_BUFFER_BIT)
-
-    glPushMatrix()
-
-    glRotatef(10, 0, 0, 1)
-    glScalef(0.7, 0.5, 1)
-    glTranslatef(0, 0, -1.0)
-    glTranslatef(0, 0.5, 0)
-
+def triangolo():
     glBegin(GL_TRIANGLES)
     glColor3f(1, 0, 0)
     glVertex3f(-1, -1, 0)
@@ -25,6 +14,21 @@ def disegna():
     glVertex3f(1, -1, 0)
     glEnd()
 
+
+def disegna(zoom):
+    glClearColor(0, 0, 0, 1)
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glTranslatef(0, 0, zoom)
+
+    glPushMatrix()
+    glRotatef(10, 0, 0, 1)
+    glScalef(0.7, 0.5, 1)
+    glTranslatef(0, 0, -1.0)
+    glTranslatef(0, 0.5, 0)
+    triangolo()
     glPopMatrix()
 
     pygame.display.flip()
@@ -37,8 +41,10 @@ def main():
     pygame.display.set_caption("3D Lab - ISIS Ponti")
     pygame.display.set_icon(pygame.image.load('icon.png'))
 
+    glMatrixMode(GL_PROJECTION)
     gluPerspective(45, width / height, 0.1, 50.0)
 
+    zoom = 0
     running = True
     while running:
         for event in pygame.event.get():
@@ -48,7 +54,14 @@ def main():
                 running = False
             if event.type == pygame.KEYUP and event.unicode == 'q':
                 running = False
-        disegna()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 4 or \
+               event.type == pygame.KEYDOWN and event.key == pygame.K_PLUS:
+                zoom += 0.5
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 5 or \
+               event.type == pygame.KEYDOWN and event.key == pygame.K_MINUS:
+                zoom -= 0.5
+        disegna(zoom)
     pygame.quit()
 
 
